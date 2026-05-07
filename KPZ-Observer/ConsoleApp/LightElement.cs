8c31ff262f34d122dbb57d9dad167588eefb0729
Melnyk Yaroslav <ipz244_myaa@student.ztu.edu.ua>
2026-05-07 18:04:43 +0300

    Додано завдання 3: Observer

---
+++ KPZ-Observer/ConsoleApp/LightElement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class LightElement : LightNode
+    {
+        public string TagName { get; set; }
+        public class EventSubscription
+        {
+            public string EventType;
+            public IEventListener Listener;
+        }
+
+        private List<EventSubscription> _subscriptions = new List<EventSubscription>();
+
+        public LightElement(string tagName)
+        {
+            TagName = tagName;
+        }
+        public void AddEventListener(string eventType, IEventListener listener)
+        {
+            EventSubscription subscription = new EventSubscription();
+            subscription.EventType = eventType;
+            subscription.Listener = listener;
+
+            _subscriptions.Add(subscription);
+        }
+
+        public void TriggerEvent(string eventType)
+        {
+            Console.WriteLine($"\n--- Симуляція події '{eventType}' для <{TagName}> ---");
+
+            foreach (EventSubscription sub in _subscriptions)
+            {
+                if (sub.EventType == eventType)
+                {
+                    sub.Listener.HandleEvent(eventType, this);
+                }
+            }
+        }
+
+        public override string OuterHtml()
+        {
+            return $"<{TagName}></{TagName}>";
+        }
+    }
+}
---
+++ KPZ-Observer/ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            LightElement button = new LightElement("button");
+            LightElement div = new LightElement("div");
+
+            IEventListener clickLogger = new ClickLogger();
+            IEventListener hoverNotifier = new HoverNotifier();
+
+            button.AddEventListener("click", clickLogger);
+            button.AddEventListener("mouseover", hoverNotifier);
+
+            div.AddEventListener("mouseover", hoverNotifier);
+
+            button.TriggerEvent("mouseover"); 
+            button.TriggerEvent("click");     
+
+            div.TriggerEvent("mouseover");    
+            div.TriggerEvent("click");        
+
+            Console.WriteLine("\nНатисніть будь-яку клавішу для виходу...");
+            Console.ReadLine();
+        }
+    }
+}
